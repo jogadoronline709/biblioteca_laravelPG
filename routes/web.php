383973de578a557a7bd5ae4c86c76297e28c25e9
jogadoronline709 <jogadoronline709@gmail.com>
2022-04-26 11:09:44 -0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -6,60 +6,59 @@ use Illuminate\Support\Facades\Route;
 
 
 
-/*
-
-|--------------------------------------------------------------------------
-
-| Web Routes
-
-|--------------------------------------------------------------------------
-
-|
-
-| Here is where you can register web routes for your application. These
-
-| routes are loaded by the RouteServiceProvider within a group which
-
-| contains the "web" middleware group. Now create something great!
-
-|
-
-*/
-
-
-
 use App\Models\Produto;
-
 use Illuminate\Http\Request;
 
 
 
 Route::get('/', function () {
-
     return view('Inicio');
 
 });
 
 
-
 Route::post('/cadastrar-produto', function (Request $request){
-
-    //dd($request->all());
 
 
 
     Produto::create([
 
         'nome' => $request ->nome,
-
         'valor' => $request ->valor,
-
         'estoque' => $request ->estoque
 
     ]);
 
-
-
     echo "Produto criado com sucesso";
 
+});
+
+Route::get('/listar-produto/{id})', function($id){
+    $produto= Produto::find($id);
+    return view('listar',['produto' => $produto]);
+
+});
+
+Route::get('/editar-produto/{id})', function($id){
+    $produto= Produto::find($id);
+    return view('editar',['produto' => $produto]);
+});
+
+Route::get('/editar-produto/{id})', function(Request $request, $id){
+    $produto = Produto::find($id);
+
+    $produto->update([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+
+    echo "Produto editado com sucesso!";
+}); 
+
+Route::get('/excluir-produto/{id})', function($id){
+    $produto= Produto::find($id);
+    $produto->delete();
+
+    echo "Produto excluido com sucesso!";
 });
